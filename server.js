8be9a4f7558beba4fc743ff798671229b5212381
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2023-01-11 12:29:52 -0500

    inital enviroment is setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+//DEPENDENCIES
+const express = require("express");
+const app = express();
 require("dotenv").config();
+const { PORT = 3000 } = process.env;
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
