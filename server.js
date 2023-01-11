1a6b0fcba3ba1e2966f8ba474e6177d07a213adc
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2023-01-11 12:59:08 -0500

    added chesse index route

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const mongoose =require("mongoose");
-const { PORT = 3000 } = process.env;
+const cors = require("cors");
+const morgan = require("morgan")
+const { PORT = 3000, MONGODB_URL } = process.env;
+
+
+
 
 //DATABASE CONNECTION
 mongoose.connect(MONGODB_URL,{
@@ -15,10 +20,35 @@ mongoose.connection
 .on("open", ()=> console.log("You are connected to Mongoose"))
 .on("close",()=> console.log("You are disconnected from mongoose"))
 .on("error",(error)=> console.log(error));
+
+//MODELS
+const ChesseSchema = new mongoose.Schema({
+    name: String,
+    countryOfOrigin: String,
+    image: String
+});
+
+const Chesse = mongoose.model("Chesse", ChesseSchema);
+
+//Middleware 
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+
 //ROUTES
+//Test Route
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+//People Index people
+app.get("/Chesse", async(req,res)=>{
+    try{
+        res.json(await Chesse.find({}));
+    } catch(error){
+        res.status(400).json(error)
+    }
+})
 
 //LISTENER
 app.listen(PORT,() => 
