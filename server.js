bd408682a387a1a010578bf8bc69c9445edef0e5
Mohamed D. Ahmed <Mohamed@Mohameds-MBP-3.fios-router.home>
2023-01-11 12:46:54 -0500

    established mongoose connection

--- server.js
+++ server.js
@@ -2,8 +2,19 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose =require("mongoose");
 const { PORT = 3000 } = process.env;
 
+//DATABASE CONNECTION
+mongoose.connect(MONGODB_URL,{
+    useUnifiedTopology: true,
+    useNewUrlParser:true,
+});
+
+mongoose.connection
+.on("open", ()=> console.log("You are connected to Mongoose"))
+.on("close",()=> console.log("You are disconnected from mongoose"))
+.on("error",(error)=> console.log(error));
 //ROUTES
 app.get("/", (req, res) => {
   res.send("hello world");
